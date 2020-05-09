74dc0e99694f27f3c2bc1a373fa262dc7c7bc4d9
Matt Elmore <github@mattelmore.com>
2020-05-08 22:08:12 -0500

    commit

--- clock.py
+++ clock.py
@@ -1,14 +1,14 @@
 import sys
-from Tkinter import *
+from tkinter import *
 import time
 
 def tick():
-    time_string = time.strftime("%H:%M:%S")
+    time_string = time.strftime("%a, %b %d\n%I:%M:%S")
     clock.config(text=time_string)
     clock.after(200,tick)
 
 root = Tk()
-clock = Label(root, font = ("times", 50, "bold"), bg="white")
+clock = Label(root, font = ("times", 20, "bold"), bg="gray")
 clock.grid(row=0, column=1)
 tick()
 root.mainloop()
